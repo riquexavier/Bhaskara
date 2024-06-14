b200afe464d18504d7ffd1b39f13de10d82e68d7
jose-tek-tek <110746209+riquexavier@users.noreply.github.com>
2024-06-14 01:12:40 -0300

    em solução

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿double a,b,c,x1,x2;
+
+Console.WriteLine("Hello, World!");
+
+Console.Write("Digite o valor de a: ");
+a = Convert.ToDouble(Console.ReadLine());
+if (a == 0)
+{
+ Console.WriteLine("a equação não é do segundo grau.");
+}
+else 
+{
+    delta = b*b -4*a*c;
+
+    if (a == 0)
+    {
+
+    }
+}
+
+
+Console.WriteLine("Digite o valor de b: ");
+b = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite o valor de c: ");
+c = Convert.ToDouble(Console.ReadLine());
+
+
