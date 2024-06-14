3ca33a16cee1507b4eedfff787f41a99e000b017
jose-tek-tek <110746209+riquexavier@users.noreply.github.com>
2024-06-14 20:16:01 -0300

    att

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
-﻿double a,b,c,x1,x2, delta;
+﻿double a, b, c, x1, x2, delta;
 Console.Clear();
 
-Console.WriteLine("Bem-Vindo ao programa Bhaskara\n");
+Console.WriteLine("Bem-vindo ao programa Bhaskara\n");  
 Console.WriteLine("Aperte qualquer tecla para iniciar o programa (: ");
 Console.ReadKey(true);
 
@@ -13,24 +13,24 @@ b = Convert.ToDouble(Console.ReadLine());
 
 Console.Write("Digite o valor de c: ");
 c = Convert.ToDouble(Console.ReadLine());
-delta = b * b -4*a*c;
-x1= (-b + Math.Sqrt(delta)) / (2 * a);
-x2= (-b - Math.Sqrt(delta)) / (2 * a);
+delta = b * b - 4 * a * c;
+x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+x2 = (-b - Math.Sqrt(delta)) / (2 * a);
 
 if (a == 0)
 {
-Console.WriteLine("A equação não é do segundo grau.");
+    Console.WriteLine("A equação não é do segundo grau.");
 }
 else
 {
     if (delta < 0)
     {
-Console.WriteLine($"Como delta é igual a {delta}, a equação não possui raízes reais.\n");
+        Console.WriteLine($"Como delta é igual a {delta}, a equação não possui raízes reais.\n");
     }
 
     if (delta > 0)
     {
-Console.WriteLine($"\nDelta é igual a {delta}.\n");
-Console.WriteLine($"Portanto x1 = {x1} e x2 = {x2}.");        
+        Console.WriteLine($"\nDelta é igual a {delta}.\n");
+        Console.WriteLine($"Portanto x1 = {x1:N2} e x2 = {x2:N2}.");
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Bhaskara
-Calcule as raízes de uma equação de segundo grau, utilizando o método de Bhaskara. 
+# *Cálculo Bhaskara*
+Este programa irá calcular uma equação de segundo grau, a partir dos valores a,b,c fornecidos pelos usuários.
+
+![alt text](image.png)
+
+## *Instalação*
+Para instalar o projeto *Cálculo Bhaskara*, siga as instruções abaixo (funcional apenas em sistemas Windows)
+
+1. Faça o download **aqui**
+2. abra o aquivo meu programa.exe
+
+Calcule as raízes de uma equação de segundo grau, utilizando o método de Bhaskara.
